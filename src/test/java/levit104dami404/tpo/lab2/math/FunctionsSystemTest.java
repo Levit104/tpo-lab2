ddc58a039e5ba0e2170ed471f23aeb1de1487ecc
Artyom <artyom0104@gmail.com>
2024-04-12 10:49:45 +0300

    Redone tests

--- src/test/java/levit104dami404/tpo/lab2/math/FunctionsSystemTest.java
+++ src/test/java/levit104dami404/tpo/lab2/math/FunctionsSystemTest.java
@@ -15,19 +15,19 @@ import java.util.List;
 public class FunctionsSystemTest {
     static final double eps = 1e-10;
 
-    static Sin sin;
-    static Cos cos;
-    static Cot cot;
-    static Ln ln;
-    static Log log2;
-    static Log log3;
-    static Log log10;
+    static Sin mockSin;
+    static Cos mockCos;
+    static Cot mockCot;
+    static Ln mockLn;
+    static Log mockLog2;
+    static Log mockLog3;
+    static Log mockLog10;
 
     static String dirName = "src/test/resources/";
     static String separator = ",";
 
-    private static void readValues(CsvHandler csvHandler, String filename, Function function) {
-        List<String> data = csvHandler.readData(dirName + filename, true);
+    private static void assignValues(CsvHandler csvHandler, String filename, Function function) {
+        List<String> data = csvHandler.readData(filename, true);
         data.forEach(row -> {
             String[] values = row.split(separator);
             Mockito.when(function.calculate(Double.parseDouble(values[0]))).thenReturn(Double.parseDouble(values[1]));
@@ -36,105 +36,74 @@ public class FunctionsSystemTest {
 
     @BeforeAll
     static void init() {
-        sin = Mockito.mock(Sin.class);
-        cos = Mockito.mock(Cos.class);
-        cot = Mockito.mock(Cot.class);
-        ln = Mockito.mock(Ln.class);
-        log2 = Mockito.mock(Log.class);
-        log3 = Mockito.mock(Log.class);
-        log10 = Mockito.mock(Log.class);
+        mockSin = Mockito.mock(Sin.class);
+        mockCos = Mockito.mock(Cos.class);
+        mockCot = Mockito.mock(Cot.class);
+        mockLn = Mockito.mock(Ln.class);
+        mockLog2 = Mockito.mock(Log.class);
+        mockLog3 = Mockito.mock(Log.class);
+        mockLog10 = Mockito.mock(Log.class);
 
         CsvHandler csvHandler = new CsvHandler(separator);
-        readValues(csvHandler, "sin.csv", sin);
-        readValues(csvHandler, "cos.csv", cos);
-        readValues(csvHandler, "cot.csv", cot);
-        readValues(csvHandler, "ln.csv", ln);
-        readValues(csvHandler, "log2.csv", log2);
-        readValues(csvHandler, "log3.csv", log3);
-        readValues(csvHandler, "log10.csv", log10);
+        assignValues(csvHandler, dirName + "sin.csv", mockSin);
+        assignValues(csvHandler, dirName + "cos.csv", mockCos);
+        assignValues(csvHandler, dirName + "cot.csv", mockCot);
+        assignValues(csvHandler, dirName + "ln.csv", mockLn);
+        assignValues(csvHandler, dirName + "log2.csv", mockLog2);
+        assignValues(csvHandler, dirName + "log3.csv", mockLog3);
+        assignValues(csvHandler, dirName + "log10.csv", mockLog10);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
     void calculateAllMocks(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(cot, ln, log2, log3, log10);
+        FunctionsSystem functionsSystem = new FunctionsSystem(mockCot, mockLn, mockLog2, mockLog3, mockLog10);
         Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
     void calculateOriginalCot(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(new Cot(sin, cos), ln, log2, log3, log10);
-        Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
-    void calculateOriginalCotAndSin(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(new Cot(new Sin(1e-20), cos), ln, log2, log3, log10);
+        FunctionsSystem functionsSystem = new FunctionsSystem(new Cot(mockSin, mockCos), mockLn, mockLog2, mockLog3, mockLog10);
         Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
     void calculateOriginalCotAndCos(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(new Cot(sin, new Cos(sin)), ln, log2, log3, log10);
+        FunctionsSystem functionsSystem = new FunctionsSystem(new Cot(mockSin, new Cos(mockSin)), mockLn, mockLog2, mockLog3, mockLog10);
         Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
-    void calculateOriginalCotAndSinAndCos(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(new Cot(new Sin(1e-20), new Cos(sin)), ln, log2, log3, log10);
+    void calculateOriginalCotAndCosAndSin(double x, double y) {
+        Sin originalSin = new Sin(1e-20);
+        FunctionsSystem functionsSystem = new FunctionsSystem(new Cot(originalSin, new Cos(originalSin)), mockLn, mockLog2, mockLog3, mockLog10);
         Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
-    void calculateOriginalLn(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(cot, new Ln(1e-20), log2, log3, log10);
+    void calculateOriginalLog(double x, double y) {
+        FunctionsSystem functionsSystem = new FunctionsSystem(mockCot, mockLn, new Log(mockLn, 2), new Log(mockLn, 3), new Log(mockLn, 10));
         Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
-    void calculateOriginalLog2(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(cot, ln, new Log(ln, 2), log3, log10);
+    void calculateOriginalLogAndLn(double x, double y) {
+        Ln originalLn = new Ln(1e-20);
+        FunctionsSystem functionsSystem = new FunctionsSystem(mockCot, originalLn, new Log(originalLn, 2), new Log(originalLn, 3), new Log(originalLn, 10));
         Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
-    void calculateOriginalLog3(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(cot, ln, log2, new Log(ln, 3), log10);
-        Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
-    void calculateOriginalLog10(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(cot, ln, log2, log3, new Log(ln, 10));
-        Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
-    void calculateOriginalLog2AndLn(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(cot, ln, new Log(new Ln(1e-20), 2), log3, log10);
-        Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
-    void calculateOriginalLog3AndLn(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(cot, ln, log2, new Log(new Ln(1e-20), 3), log10);
-        Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/function.csv", numLinesToSkip = 1)
-    void calculateOriginalLog10AndLn(double x, double y) {
-        FunctionsSystem functionsSystem = new FunctionsSystem(cot, ln, log2, log3, new Log(new Ln(1e-20), 10));
+    void calculateOriginalAll(double x, double y) {
+        Sin originalSin = new Sin(1e-20);
+        Ln originalLn = new Ln(1e-20);
+        FunctionsSystem functionsSystem = new FunctionsSystem(new Cot(originalSin, new Cos(originalSin)), originalLn, new Log(originalLn, 2), new Log(originalLn, 3), new Log(originalLn, 10));
         Assertions.assertEquals(y, functionsSystem.calculate(x), eps);
     }
 }
